c8abfb41a979e4b31875be38600a412b9b77c7d5
Michelle Beshears <meeshysu@gmail.com>
2018-09-22 19:51:27 -0500

    both breaker and maker work

--- main.js
+++ main.js
@@ -12,17 +12,27 @@ const makeMaker = () => {
     // let codeMaker = sentence.charCodeAt(0);
     let sentence = document.getElementById('inputElem').value;
     let sentenceSplit = sentence.split(' ');
-for (let i = 0; i < sentence.length; i++) {
-    outputString = sentence.charCodeAt(i) + ",";
+for (let i = 0; i < sentenceSplit.length; i++) {
+    outputString = sentenceSplit.charCodeAt(i) + ",";
     printToDom(outputString, 'emptySpace');
 } 
 }
 
 
 
+const breakBreaker = () => {
+    sentenceString = ',';
+    let sentence = document.getElementById('inputElem2').value;
+    let sentenceBreaker = sentence.split(',');
+    for (let i = 0; i < sentenceBreaker.length; i++) {
+        sentenceString = String.fromCharCode(sentenceBreaker[i]);
+        printToDom(sentenceString, 'alsoEmptySpace');
+    }
+}
+
+
 let submitButton = document.getElementById("submitMe");
 submitButton.addEventListener('click', function() {makeMaker()});
 
-
-
-
+let submitButtonToo = document.getElementById('submitMeToo');
+submitButtonToo.addEventListener('click', function() {breakBreaker()});
